71e0ddae50e98e6ff85809ae5c403dd23591c2f1
faha1999 <kafahad1999@gmail.com>
2022-03-06 17:49:25 +0600

    added nav

--- 1. First react/7. Custom Components challenge/app.js
+++ 1. First react/7. Custom Components challenge/app.js
@@ -1,4 +1,4 @@
-// part 1
+// // part 1
 
 // function Page() {
 //   return (
@@ -13,7 +13,7 @@
 
 // ReactDOM.render(<Page />, document.getElementById('root'));
 
-// part 2
+// // part 2
 
 // function Page() {
 //   return (
@@ -39,12 +39,63 @@
 
 // ReactDOM.render(<Page />, document.getElementById('root'));
 
-// Part 3 (Parent/Child Components)
+// // Part 3 (Parent/Child Components)
+// function Header() {
+//   return (
+//     <header>
+//       <nav>
+//         <img src="./react-logo.png" width="40px" />
+//       </nav>
+//     </header>
+//   );
+// }
+
+// function Footer() {
+//   return (
+//     <footer>
+//       <small>© 2022 faha1999 development. All rights reserved.</small>
+//     </footer>
+//   );
+// }
+
+// function MainContent() {
+//   return (
+//     <div>
+//       <h1>Reasons I'm excited to learn React</h1>
+//       <ol>
+//         <li>
+//           It's a popular library, so I'll be able to fit in with the cool kids!
+//         </li>
+//         <li>I'm more likely to get a job as a developer if I know React</li>
+//       </ol>
+//     </div>
+//   );
+// }
+
+// function Page() {
+//   return (
+//     <div>
+//       <Header />
+//       <MainContent />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<Page />, document.getElementById('root'));
+
+// // part 4 (adding nav)
+
 function Header() {
   return (
     <header>
-      <nav>
-        <img src="./react-logo.png" width="40px" />
+      <nav className="nav">
+        <img src="./react-logo.png" className="nav-logo" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
       </nav>
     </header>
   );

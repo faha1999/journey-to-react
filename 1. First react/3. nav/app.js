00114f610fc78bb1c13c701bfa539aa7feaee779
faha1999 <kafahad1999@gmail.com>
2022-03-06 13:36:38 +0600

    complete nav by jsx

---
+++ 1. First react/3. nav/app.js
@@ -0,0 +1,13 @@
+const navbar = (
+  <nav>
+    <h1>This is Fahad</h1>
+
+    <ul>
+      <li>Menu</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+
+ReactDOM.render(navbar, document.getElementById('root'));

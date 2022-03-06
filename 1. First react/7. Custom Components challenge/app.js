fa73ab6d376b836c0da3251618aa9b1cd6286c5a
faha1999 <kafahad1999@gmail.com>
2022-03-06 15:34:25 +0600

    Parent/Child Components

--- 1. First react/7. Custom Components challenge/app.js
+++ 1. First react/7. Custom Components challenge/app.js
@@ -15,14 +15,52 @@
 
 // part 2
 
-function Page() {
+// function Page() {
+//   return (
+//     <div>
+//       <header>
+//         <nav>
+//           <img src="./react-logo.png" width="40px" />
+//         </nav>
+//       </header>
+//       <h1>Reasons I'm excited to learn React</h1>
+//       <ol>
+//         <li>
+//           It's a popular library, so I'll be able to fit in with the cool kids!
+//         </li>
+//         <li>I'm more likely to get a job as a developer if I know React</li>
+//       </ol>
+//       <footer>
+//         <small>© 2022 faha1999 development. All rights reserved.</small>
+//       </footer>
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<Page />, document.getElementById('root'));
+
+// Part 3 (Parent/Child Components)
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./react-logo.png" width="40px" />
+      </nav>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2022 faha1999 development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function MainContent() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="./react-logo.png" width="40px" />
-        </nav>
-      </header>
       <h1>Reasons I'm excited to learn React</h1>
       <ol>
         <li>
@@ -30,9 +68,16 @@ function Page() {
         </li>
         <li>I'm more likely to get a job as a developer if I know React</li>
       </ol>
-      <footer>
-        <small>© 2022 faha1999 development. All rights reserved.</small>
-      </footer>
+    </div>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }

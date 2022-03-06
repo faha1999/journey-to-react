77f3915444f06fd716ee49857232cdcc468aa486
faha1999 <kafahad1999@gmail.com>
2022-03-06 18:13:34 +0600

    done

---
+++ 1. First react/8. Organize components/app.js
@@ -0,0 +1,15 @@
+import Header from './Header';
+import Footer from './Footer';
+import MainContent from './MainContent';
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+ReactDOM.render(<Page />, document.getElementById('root'));

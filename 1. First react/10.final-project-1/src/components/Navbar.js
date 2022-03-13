30bf94831feb67c3f18c87a4174a2591d5537f39
faha1999 <kafaha1999@gmail.com>
2022-03-13 20:33:27 +0600

    solved logo issues

--- 1. First react/10.final-project-1/src/components/Navbar.js
+++ 1. First react/10.final-project-1/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import logo from '../images/react-logo.png';
 
 export default function Navbar() {
   return (
     <nav>
-      <img src="../images/react-logo.png" className="nav--icon" />
+      <img src={logo} className="nav--icon" />
       <h3 className="nav--logo_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>

aae008660ff3ba86a65371c4170037368f20f85e
faha1999 <kafaha1999@gmail.com>
2022-03-13 19:13:01 +0600

    innitial setup done

---
+++ 1. First react/10.final-project-1/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+
+ReactDOM.render(
+  <h1>Hello, I just create React App!</h1>,
+  document.getElementById('root')
+);

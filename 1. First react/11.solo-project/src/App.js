198c64f72e614af754fbbfcec81e0a0a51963908
faha1999 <kafaha1999@gmail.com>
2022-03-14 22:17:32 +0600

    innitian setup done

---
+++ 1. First react/11.solo-project/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import css from './css/style.min.css';
+
+export default function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}

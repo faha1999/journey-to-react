6041bce432f8c07f34c422efb48d377c740547d0
faha1999 <kafaha1999@gmail.com>
2022-03-15 00:02:31 +0600

    complate main

--- 11.solo-project/src/components/Main.js
+++ 11.solo-project/src/components/Main.js
@@ -3,14 +3,22 @@ import React from 'react';
 export default function Main() {
   return (
     <main>
-      {/* <h1 className="main--title">Fun facts about React</h1>
-      <ul className="main--facts">
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul> */}
+      <div className="about">
+        <h1>About</h1>
+        <p>
+          I am a frontend developer with a particular interest in making things
+          simple and automating daily tasks. I try to keep up with security and
+          best practices, and am always looking for new things to learn.
+        </p>
+      </div>
+
+      <div className="about">
+        <h1>Interests</h1>
+        <p>
+          Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+          Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+        </p>
+      </div>
     </main>
   );
 }

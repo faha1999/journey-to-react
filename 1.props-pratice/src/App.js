0b97153eca7938d1f48e04d1fd05d551eab60053
faha1999 <kafaha1999@gmail.com>
2022-03-16 20:33:22 +0600

    INNITIAL

---
+++ 1.props-pratice/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Joke from './Joke';
+
+export default function App() {
+  return (
+    <div>
+      <Joke />
+      <Joke />
+      <Joke />
+      <Joke />
+      <Joke />
+      <Joke />
+    </div>
+  );
+}

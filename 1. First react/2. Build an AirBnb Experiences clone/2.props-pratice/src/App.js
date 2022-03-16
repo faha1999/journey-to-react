a0c268e4e285307cc35e92c02920be3579b80f7a
faha1999 <kafaha1999@gmail.com>
2022-03-17 00:36:27 +0600

    applied map() in props

--- 1. First react/2. Build an AirBnb Experiences clone/2.props-pratice/src/App.js
+++ 1. First react/2. Build an AirBnb Experiences clone/2.props-pratice/src/App.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import Joke from './Joke';
+import jokesData from './jokesData';
 
 export default function App() {
-  return (
-    <div>
-      <Joke punchline="I can't wait to see her face light up when she opens it." />
+  const jokeElements = jokesData.map((joke) => {
+    return <Joke setup={joke.setup} punchline={joke.punchline} />;
+  });
+  return <div>{jokeElements}</div>;
+}
 
-      <Joke
-        setup="I got my daughter a fridge for her birthday."
-        punchline="I can't wait to see her face light up when she opens it."
-      />
-      <Joke
-        setup="How did the hacker escape the police?"
-        punchline="He just ransomware!"
-      />
-      <Joke
-        setup="Why don't pirates travel on mountain roads?"
-        punchline="Scurvy."
-      />
-      <Joke
-        setup="Why do bees stay in the hive in the winter?"
-        punchline="Swarm."
-      />
-      <Joke
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is a big plus!"
-      />
-    </div>
-  );
+{
+  /* <Joke punchline="I can't wait to see her face light up when she opens it." />
+
+<Joke
+  setup="I got my daughter a fridge for her birthday."
+  punchline="I can't wait to see her face light up when she opens it."
+/>
+<Joke
+  setup="How did the hacker escape the police?"
+  punchline="He just ransomware!"
+/>
+<Joke
+  setup="Why don't pirates travel on mountain roads?"
+  punchline="Scurvy."
+/>
+<Joke
+  setup="Why do bees stay in the hive in the winter?"
+  punchline="Swarm."
+/>
+<Joke
+  setup="What's the best thing about Switzerland?"
+  punchline="I don't know, but the flag is a big plus!"
+/> */
 }

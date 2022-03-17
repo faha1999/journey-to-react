2883ce32786be5e14369e406c990fe7ca2a2b276
faha1999 <kafaha1999@gmail.com>
2022-03-18 01:39:43 +0600

    completed AirBnb

--- 1. First react/2. Build an AirBnb Experiences clone/airbmb/src/App.js
+++ 1. First react/2. Build an AirBnb Experiences clone/airbmb/src/App.js
@@ -8,18 +8,7 @@ import css from './css/style.min.css';
 
 export default function App() {
   const cards = data.map((item) => {
-    return (
-      <Card
-        key={item.id}
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
-        openSpots={item.openSpots}
-      />
-    );
+    return <Card key={item.id} item={item} />;
   });
 
   return (
--- 1. First react/2. Build an AirBnb Experiences clone/airbmb/src/components/Card.js
+++ 1. First react/2. Build an AirBnb Experiences clone/airbmb/src/components/Card.js
@@ -3,26 +3,26 @@ import star from '../images/star.png';
 
 export default function Card(props) {
   let badgeText;
-  if (props.openSpots === 0) {
+  if (props.item.openSpots === 0) {
     badgeText = 'SOLD OUT';
-  } else if (props.location === 'Online') {
+  } else if (props.item.location === 'Online') {
     badgeText = 'ONLINE';
   }
 
   return (
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
-      <img src={`../images/${props.img}`} className="card--image" />
+      <img src={`../images/${props.item.coverImg}`} className="card--image" />
 
       <div className="card--stats">
         <img src={star} className="card--star" />
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
-        <span className="gray">{props.location}</span>
+        <span>{props.item.stats.rating}</span>
+        <span className="gray">({props.item.stats.reviewCount}) • </span>
+        <span className="gray">{props.item.location}</span>
       </div>
-      <p className="card--title">{props.title}</p>
+      <p className="card--title">{props.item.title}</p>
       <p className="card--price">
-        <strong>From ${props.price} / </strong>person
+        <strong>From ${props.item.price} / </strong>person
       </p>
     </div>
   );

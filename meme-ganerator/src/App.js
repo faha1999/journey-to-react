2ff6e630ef9a53c0d2a93c2438a74c6ac025b4c9
faha1999 <kafaha1999@gmail.com>
2022-03-21 22:57:24 +0600

    added input box

--- meme-ganerator/src/App.js
+++ meme-ganerator/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Meme from './components/Meme';
 
 import css from './css/style.min.css';
 
@@ -7,6 +8,7 @@ export default function App() {
   return (
     <div>
       <Header />
+      <Meme />
     </div>
   );
 }

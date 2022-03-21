af032e6b57e16c63389db3572b6e0f4ad735dda0
faha1999 <kafaha1999@gmail.com>
2022-03-22 00:53:09 +0600

    added data

--- meme-ganerator/src/components/Meme.js
+++ meme-ganerator/src/components/Meme.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import memesData from '../memesData';
 
 export default function Meme() {
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    url = memesArray[randomNumber].url;
+    console.log(url);
+  }
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
         <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+      </div>
     </main>
   );
 }

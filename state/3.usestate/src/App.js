a4a04270b915dc325c35c046d86b8f1cd28ae3ce
faha1999 <kafaha1999@gmail.com>
2022-03-23 23:21:58 +0600

    added ternary

--- state/3.usestate/src/App.js
+++ state/3.usestate/src/App.js
@@ -2,17 +2,13 @@ import React from 'react';
 import css from './css/style.min.css';
 
 export default function App() {
-  const [isImportant, setIsImportant] = React.useState('Yes');
-
-  function handleClick() {
-    setIsImportant('No');
-  }
+  const isGoingOut = false;
 
   return (
     <div className="state">
-      <h1 className="state--title">Is state important to know?</h1>
-      <div className="state--value" onClick={handleClick}>
-        <h1>{isImportant}</h1>
+      <h1 className="state--title">Do I feel like going out tonight?</h1>
+      <div className="state--value">
+        <h1>{isGoingOut ? 'Yes' : 'No'}</h1>
       </div>
     </div>
   );

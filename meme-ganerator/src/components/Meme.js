d4abf739e5b4ca015090f209e1598cf6b59aff43
faha1999 <kafaha1999@gmail.com>
2022-03-23 22:48:36 +0600

    add image function

--- meme-ganerator/src/components/Meme.js
+++ meme-ganerator/src/components/Meme.js
@@ -2,11 +2,12 @@ import React from 'react';
 import memesData from '../memesData';
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState('');
+
   function getMemeImage() {
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    url = memesArray[randomNumber].url;
-    console.log(url);
+    setMemeImage(memesArray[randomNumber].url);
   }
 
   return (
@@ -14,8 +15,11 @@ export default function Meme() {
       <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
-        <button className="form--button">Get a new meme image 🖼</button>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
       </div>
+      <img src={memeImage} className="meme--image" />
     </main>
   );
 }

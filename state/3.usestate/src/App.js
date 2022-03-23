d384a0c723797f6f3bee1511cdbcb538bf0f4973
faha1999 <kafaha1999@gmail.com>
2022-03-24 00:16:51 +0600

    applied flipping state

--- state/3.usestate/src/App.js
+++ state/3.usestate/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import css from './css/style.min.css';
 
 export default function App() {
-  const isGoingOut = false;
+  const [isGoingOut, setIsGoingOut] = React.useState(true);
+
+  function changeMind() {
+    setIsGoingOut((prevState) => !prevState);
+  }
 
   return (
     <div className="state">
       <h1 className="state--title">Do I feel like going out tonight?</h1>
-      <div className="state--value">
+      <div onClick={changeMind} className="state--value">
         <h1>{isGoingOut ? 'Yes' : 'No'}</h1>
       </div>
     </div>

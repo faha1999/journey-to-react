e92e6f78fd275e2a5bbe2c7c4c5d0e13f6166c65
faha1999 <kafaha1999@gmail.com>
2022-03-24 21:59:25 +0600

    applied

--- state/5.state-object/src/App.js
+++ state/5.state-object/src/App.js
@@ -1,7 +1,47 @@
 import React from 'react';
+// import starFilled from './images/star-filled.png';
+// import starEmpty from './images/star-empty.png';
 
-function App() {
-  return <div className="App"></div>;
+// import './images/star-filled.png';
+// import './images/star-empty.png';
+
+import user from './images/user.png';
+import './css/style.min.css';
+
+export default function App() {
+  const [contact, setContact] = React.useState({
+    firstName: 'John',
+    lastName: 'Doe',
+    phone: '+1 (719) 555-1212',
+    email: 'itsmyrealname@example.com',
+    isFavorite: true
+  });
+
+  let starIcon = contact.isFavorite
+    ? './images/star-filled.png'
+    : './images/star-empty.png';
+
+  function toggleFavorite() {
+    console.log('Toggle Favorite');
+  }
+
+  return (
+    <main>
+      <article className="card">
+        <img src={user} className="card--image" />
+        <div className="card--info">
+          <img
+            src={starIcon}
+            className="card--favorite"
+            onClick={toggleFavorite}
+          />
+          <h2 className="card--name">
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="card--contact">{contact.phone}</p>
+          <p className="card--contact">{contact.email}</p>
+        </div>
+      </article>
+    </main>
+  );
 }
-
-export default App;

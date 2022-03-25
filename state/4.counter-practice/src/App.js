c43e6236e1d968d4176717caeb24acf95d7586ca
faha1999 <kafaha1999@gmail.com>
2022-03-25 21:58:51 +0600

    tested pasing state as props

--- state/4.counter-practice/src/App.js
+++ state/4.counter-practice/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Count from './Count';
 
 export default function App() {
   const [count, setCount] = React.useState(0);
@@ -6,18 +7,19 @@ export default function App() {
   function add() {
     setCount((prevCount) => prevCount + 1);
   }
+
   function subtract() {
     setCount((prevCount) => prevCount - 1);
   }
+
+  console.log('App component rendered');
 
   return (
     <div className="counter">
       <button className="counter--minus" onClick={subtract}>
         –
       </button>
-      <div className="counter--count">
-        <h1>{count}</h1>
-      </div>
+      <Count number={count} />
       <button className="counter--plus" onClick={add}>
         +
       </button>

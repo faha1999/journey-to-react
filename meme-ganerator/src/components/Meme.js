b2e696ed09669b350a77fd6857d790a745ec7ca2
faha1999 <kafaha1999@gmail.com>
2022-03-25 21:40:51 +0600

    refactor state

--- meme-ganerator/src/components/Meme.js
+++ meme-ganerator/src/components/Meme.js
@@ -2,12 +2,21 @@ import React from 'react';
 import memesData from '../memesData';
 
 export default function Meme() {
-  const [memeImage, setMemeImage] = React.useState('');
+  const [meme, setMeme] = React.useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'http://i.imgflip.com/1bij.jpg'
+  });
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
   function getMemeImage() {
-    const memesArray = memesData.data.memes;
+    const memesArray = allMemeImages.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    setMemeImage(memesArray[randomNumber].url);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url
+    }));
   }
 
   return (
@@ -19,7 +28,7 @@ export default function Meme() {
           Get a new meme image 🖼
         </button>
       </div>
-      <img src={memeImage} className="meme--image" />
+      <img src={meme.randomImage} className="meme--image" />
     </main>
   );
 }

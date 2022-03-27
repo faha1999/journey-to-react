1197adef37c11a4496ef01c74fce183ed1878175
faha1999 <kafaha1999@gmail.com>
2022-03-27 22:24:51 +0600

    completed setting state

--- state/7.box-challenge/src/App.js
+++ state/7.box-challenge/src/App.js
@@ -6,27 +6,40 @@ import Box from './Box';
 export default function App() {
   const [squares, setSquares] = React.useState(boxes);
 
+  // function toggle(id) {
+  //   setSquares((prevSquares) => {
+  //     const newSquares = [];
+  //     for (let i = 0; i < prevSquares.length; i++) {
+  //       const currentSquare = prevSquares[i];
+  //       if (currentSquare.id === id) {
+  //         const updatedSquare = {
+  //           ...currentSquare,
+  //           on: !currentSquare.on
+  //         };
+  //         newSquares.push(updatedSquare);
+  //       } else {
+  //         newSquares.push(currentSquare);
+  //       }
+  //     }
+  //     return newSquares;
+  //   });
+  // }
+
   function toggle(id) {
     setSquares((prevSquares) => {
-      const newSquares = [];
-      for (let i = 0; i < prevSquares.length; i++) {
-        const currentSquare = prevSquares[i];
-        if (currentSquare.id === id) {
-          const updatedSquare = {
-            ...currentSquare,
-            on: !currentSquare.on
-          };
-          newSquares.push(updatedSquare);
-        } else {
-          newSquares.push(currentSquare);
-        }
-      }
-      return newSquares;
+      return prevSquares.map((square) => {
+        return square.id === id ? { ...square, on: !square.on } : square;
+      });
     });
   }
 
   const squareElements = squares.map((square) => (
-    <Box key={square.id} id={square.id} on={square.on} toggle={toggle} />
+    <Box
+      key={square.id}
+      id={square.id}
+      on={square.on}
+      toggle={() => toggle(square.id)}
+    />
   ));
 
   return <main>{squareElements}</main>;
--- state/7.box-challenge/src/Box.js
+++ state/7.box-challenge/src/Box.js
@@ -5,11 +5,5 @@ export default function Box(props) {
     backgroundColor: props.on ? '#222222' : 'transparent'
   };
 
-  return (
-    <div
-      style={styles}
-      className="box"
-      onClick={() => props.toggle(props.id)}
-    ></div>
-  );
+  return <div style={styles} className="box" onClick={props.toggle}></div>;
 }

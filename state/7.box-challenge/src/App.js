25bc33d5e061bb26023ad2ba84bb10a0cb8d3714
faha1999 <kafaha1999@gmail.com>
2022-03-27 19:33:59 +0600

    unified state

--- state/7.box-challenge/src/App.js
+++ state/7.box-challenge/src/App.js
@@ -6,8 +6,12 @@ import Box from './Box';
 export default function App() {
   const [squares, setSquares] = React.useState(boxes);
 
+  function toggle(id) {
+    console.log(id);
+  }
+
   const squareElements = squares.map((square) => (
-    <Box key={square.id} on={square.on} />
+    <Box key={square.id} id={square.id} on={square.on} toggle={toggle} />
   ));
 
   return <main>{squareElements}</main>;

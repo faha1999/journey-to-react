171c5385f7285fa0552f6148e16703d01017e93a
faha1999 <kafaha1999@gmail.com>
2022-03-28 21:55:16 +0600

    && practice

--- Conditional rendering/1. &&/src/App.js
+++ Conditional rendering/1. &&/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './css/style.min.css';
-import Joke from './Joke';
-import jokesData from './jokesData';
+// import Joke from './Joke';
+// import jokesData from './jokesData';
 
+// export default function App() {
+//   const jokeElements = jokesData.map((joke) => {
+//     return <Joke key={joke.id} setup={joke.setup} punchline={joke.punchline} />;
+//   });
+//   return <div>{jokeElements}</div>;
+// }
+
+// && practice
 export default function App() {
-  const jokeElements = jokesData.map((joke) => {
-    return <Joke key={joke.id} setup={joke.setup} punchline={joke.punchline} />;
-  });
-  return <div>{jokeElements}</div>;
+  const [messages, setMessages] = React.useState(['a', 'b']);
+
+  return (
+    <div>
+      {messages.length > 0 && (
+        <h1>You have {messages.length} unread messages!</h1>
+      )}
+    </div>
+  );
 }

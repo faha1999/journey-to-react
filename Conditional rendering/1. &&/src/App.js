95e613765b07a4c3c79fe3c32a41126d8b9f9e96
faha1999 <kafaha1999@gmail.com>
2022-03-28 22:04:32 +0600

    applied &&

--- Conditional rendering/1. &&/src/App.js
+++ Conditional rendering/1. &&/src/App.js
@@ -12,12 +12,29 @@ import './css/style.min.css';
 
 // && practice
 export default function App() {
-  const [messages, setMessages] = React.useState(['a', 'b']);
+  const [messages, setMessages] = React.useState([
+    'a',
+    'b',
+    'test',
+    '2',
+    'again'
+  ]);
+
+  const [messagesBody, setMessagesBody] = React.useState([
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  ]);
 
   return (
     <div>
       {messages.length > 0 && (
         <h1>You have {messages.length} unread messages!</h1>
+      )}
+
+      {messagesBody.length > 0 && (
+        <p>
+          You have <strong>{messagesBody.length}</strong> important messages!
+        </p>
       )}
     </div>
   );

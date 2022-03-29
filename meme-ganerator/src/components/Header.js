31b8ff9aa23b57dbd7df5a30ee38212a5b58470d
faha1999 <kafaha1999@gmail.com>
2022-03-30 01:05:12 +0600

    added text to image

--- meme-ganerator/src/components/Header.js
+++ meme-ganerator/src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="header">
       <img src={trollFace} className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
-      <h4 className="header--project">React Course - Project 3</h4>
+      <h4 className="header--project">React - Project 3</h4>
     </header>
   );
 }

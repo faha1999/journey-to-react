9123a1bdb5a57196c4f9796c1833e14a7551a077
faha1999 <kafaha1999@gmail.com>
2022-03-29 21:32:21 +0600

    tetsed Select & Option

--- 4. Form/1.input/src/Form.js
+++ 4. Form/1.input/src/Form.js
@@ -8,11 +8,13 @@ export default function Form() {
     email: '',
     comments: '',
     isFriendly: true,
-    employment: ''
+    employment: '',
+    favColor: ''
   });
-  console.log(formData.employment);
+  console.log(formData.favColor);
 
   function handleChange(event) {
+    console.log(event);
     const { name, value, type, checked } = event.target;
     setFormData((prevFormData) => {
       return {
@@ -64,7 +66,6 @@ export default function Form() {
 
       <fieldset>
         <legend>Current employment status</legend>
-
         <input
           type="radio"
           id="unemployed"
@@ -98,6 +99,25 @@ export default function Form() {
         <label htmlFor="full-time">Full-time</label>
         <br />
       </fieldset>
+      <br />
+
+      <label htmlFor="favColor">What is your favorite color?</label>
+      <br />
+      <select
+        id="favColor"
+        value={formData.favColor}
+        onChange={handleChange}
+        name="favColor"
+      >
+        <option value="">-- Choose --</option>
+        <option value="red">Red</option>
+        <option value="orange">Orange</option>
+        <option value="yellow">Yellow</option>
+        <option value="green">Green</option>
+        <option value="blue">Blue</option>
+        <option value="indigo">Indigo</option>
+        <option value="violet">Violet</option>
+      </select>
     </form>
   );
 }

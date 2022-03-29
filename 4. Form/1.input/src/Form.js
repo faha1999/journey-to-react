6b19a686c1c537c3bbf1d5e29292f3e6e60482ec
faha1999 <kafaha1999@gmail.com>
2022-03-29 18:44:48 +0600

    tested from state objects

---
+++ 4. Form/1.input/src/Form.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './css/style.min.css';
+
+export default function Form() {
+  const [formData, setFormData] = React.useState({
+    firstName: '',
+    lastName: ''
+  });
+
+  console.log(formData);
+
+  function handleChange(event) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value
+      };
+    });
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+        name="firstName"
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+      />
+    </form>
+  );
+}

40df914024173db7aab794713b622847a70b7d84
faha1999 <kafaha1999@gmail.com>
2022-03-30 00:04:10 +0600

    complete sign up from

--- 4. Form/2.sign-up-form/src/App.js
+++ 4. Form/2.sign-up-form/src/App.js
@@ -2,8 +2,33 @@ import React from 'react';
 import './css/style.min.css';
 
 export default function App() {
+  const [formData, setFormData] = React.useState({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    joinedNewsletter: true
+  });
+
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: type === 'checkbox' ? checked : value
+    }));
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
+    if (formData.password === formData.passwordConfirm) {
+      console.log('Successfully signed up');
+    } else {
+      console.log('Passwords do not match');
+      return;
+    }
+
+    if (formData.joinedNewsletter) {
+      console.log('Thanks for signing up for our newsletter!');
+    }
   }
 
   return (
@@ -13,16 +38,35 @@ export default function App() {
           type="email"
           placeholder="Email address"
           className="form--input"
+          name="email"
+          onChange={handleChange}
+          value={formData.email}
         />
-        <input type="password" placeholder="Password" className="form--input" />
+        <input
+          type="password"
+          placeholder="Password"
+          className="form--input"
+          name="password"
+          onChange={handleChange}
+          value={formData.password}
+        />
         <input
           type="password"
           placeholder="Confirm password"
           className="form--input"
+          name="passwordConfirm"
+          onChange={handleChange}
+          value={formData.passwordConfirm}
         />
 
         <div className="form--marketing">
-          <input id="okayToEmail" type="checkbox" />
+          <input
+            id="okayToEmail"
+            type="checkbox"
+            name="joinedNewsletter"
+            onChange={handleChange}
+            checked={formData.joinedNewsletter}
+          />
           <label htmlFor="okayToEmail">I want to join the newsletter</label>
         </div>
         <button className="form--submit">Sign up</button>

b31487e64b622ede55994337c6d7d7c32197bb64
faha1999 <kafaha1999@gmail.com>
2022-03-29 21:10:28 +0600

    applied radio nutton

--- 4. Form/1.input/src/Form.js
+++ 4. Form/1.input/src/Form.js
@@ -7,8 +7,10 @@ export default function Form() {
     lastName: '',
     email: '',
     comments: '',
-    isFriendly: true
+    isFriendly: true,
+    employment: ''
   });
+  console.log(formData.employment);
 
   function handleChange(event) {
     const { name, value, type, checked } = event.target;
@@ -58,6 +60,44 @@ export default function Form() {
       />
       <label htmlFor="isFriendly">Are you friendly?</label>
       <br />
+      <br />
+
+      <fieldset>
+        <legend>Current employment status</legend>
+
+        <input
+          type="radio"
+          id="unemployed"
+          name="employment"
+          value="unemployed"
+          checked={formData.employment === 'unemployed'}
+          onChange={handleChange}
+        />
+        <label htmlFor="unemployed">Unemployed</label>
+        <br />
+
+        <input
+          type="radio"
+          id="part-time"
+          name="employment"
+          value="part-time"
+          checked={formData.employment === 'part-time'}
+          onChange={handleChange}
+        />
+        <label htmlFor="part-time">Part-time</label>
+        <br />
+
+        <input
+          type="radio"
+          id="full-time"
+          name="employment"
+          value="full-time"
+          checked={formData.employment === 'full-time'}
+          onChange={handleChange}
+        />
+        <label htmlFor="full-time">Full-time</label>
+        <br />
+      </fieldset>
     </form>
   );
 }

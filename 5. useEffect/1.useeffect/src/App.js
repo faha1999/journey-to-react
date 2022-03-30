9617d7e99093a0ab7e064fc15f46cab68e56fd4b
faha1999 <kafaha1999@gmail.com>
2022-03-31 00:37:11 +0600

    used dependencies array

--- 5. useEffect/1.useeffect/src/App.js
+++ 5. useEffect/1.useeffect/src/App.js
@@ -19,21 +19,43 @@ import './css/style.min.css';
 //   );
 // }
 
-// // syntax and default behavior
+// // // syntax and default behavior
+// export default function App() {
+//   const [starWarsData, setStarWarsData] = React.useState({});
+
+//   console.log('Component rendered');
+
+//   // side effects
+//   React.useEffect(function () {
+//     fetch('https://swapi.dev/api/people/1').then((res) => res.json());
+//     // .then(data => setStarWarsData(data))
+//   });
+
+//   return (
+//     <div>
+//       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+//     </div>
+//   );
+// }
+
+// // useEffect dependencies array
 export default function App() {
   const [starWarsData, setStarWarsData] = React.useState({});
+  const [count, setCount] = React.useState(0);
 
   console.log('Component rendered');
 
-  // side effects
-  React.useEffect(function () {
-    fetch('https://swapi.dev/api/people/1').then((res) => res.json());
-    // .then(data => setStarWarsData(data))
-  });
+  React.useEffect(() => {
+    console.log('Effect function ran');
+  }, [count]);
 
   return (
     <div>
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+      <h2>The count is {count}</h2>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Add
+      </button>
     </div>
   );
 }

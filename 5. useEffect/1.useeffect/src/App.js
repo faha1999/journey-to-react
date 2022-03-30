dc195d8b65fca3a46c60ea09009001fc60b9bfe4
faha1999 <kafaha1999@gmail.com>
2022-03-30 23:37:22 +0600

    tested syntax and default behavior

--- 5. useEffect/1.useeffect/src/App.js
+++ 5. useEffect/1.useeffect/src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import './css/style.min.css';
 
+// export default function App() {
+//   const [starWarsData, setStarWarsData] = React.useState({});
+
+//   // console.log("Component rendered")
+
+//   // fetch("https://swapi.dev/api/people/1")
+//   //     .then(res => res.json())
+//   //     .then(data => setStarWarsData(data))
+
+//   // side effects
+
+//   return (
+//     <div>
+//       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+//     </div>
+//   );
+// }
+
+// // syntax and default behavior
 export default function App() {
   const [starWarsData, setStarWarsData] = React.useState({});
 
-  // console.log("Component rendered")
-
-  // fetch("https://swapi.dev/api/people/1")
-  //     .then(res => res.json())
-  //     .then(data => setStarWarsData(data))
+  console.log('Component rendered');
 
   // side effects
+  React.useEffect(function () {
+    fetch('https://swapi.dev/api/people/1').then((res) => res.json());
+    // .then(data => setStarWarsData(data))
+  });
 
   return (
     <div>

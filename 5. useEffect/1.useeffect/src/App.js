072b63610d125ce9bed3cd34f3c677053ea61793
faha1999 <kafaha1999@gmail.com>
2022-04-01 01:28:21 +0600

    used dependencies

--- 5. useEffect/1.useeffect/src/App.js
+++ 5. useEffect/1.useeffect/src/App.js
@@ -60,26 +60,53 @@ import './css/style.min.css';
 //   );
 // }
 
-// //  fetching data
+// // //  fetching data
+
+// export default function App() {
+//   const [starWarsData, setStarWarsData] = React.useState({});
+//   const [count, setCount] = React.useState(0);
+
+//   React.useEffect(function () {
+//     console.log('Effect ran');
+//     fetch('https://swapi.dev/api/people/1')
+//       .then((res) => res.json())
+//       .then((data) => setStarWarsData(data));
+//   }, []);
+
+//   return (
+//     <div>
+//       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+//       <h2>The count is {count}</h2>
+//       <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+//         Add
+//       </button>
+//     </div>
+//   );
+// }
+
+// //  use dependencies
 
 export default function App() {
   const [starWarsData, setStarWarsData] = React.useState({});
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = React.useState(1);
 
-  React.useEffect(function () {
-    console.log('Effect ran');
-    fetch('https://swapi.dev/api/people/1')
-      .then((res) => res.json())
-      .then((data) => setStarWarsData(data));
-  }, []);
+  React.useEffect(
+    function () {
+      console.log('Effect ran');
+      fetch(`https://swapi.dev/api/people/${count}`)
+        .then((res) => res.json())
+        .then((data) => setStarWarsData(data));
+    },
+    [count]
+  );
 
   return (
     <div>
-      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
       <h2>The count is {count}</h2>
       <button onClick={() => setCount((prevCount) => prevCount + 1)}>
-        Add
+        Get Next Character
       </button>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
 }

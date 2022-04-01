82819cd60a1d84f7ee7dd3e7b2d5a884b9ba65d1
faha1999 <kafaha1999@gmail.com>
2022-04-01 21:17:56 +0600

    applied state, useEffect

--- 5. useEffect/2.stateandeffectpractice/src/App.js
+++ 5. useEffect/2.stateandeffectpractice/src/App.js
@@ -3,17 +3,16 @@ import WindowTracker from './WindowTracker';
 import './css/style.min.css';
 
 export default function App() {
-  /**
-   * Challenge:
-   * 1. Create state called `show`, default to `true`
-   * 2. When the button is clicked, toggle `show`
-   * 3. Only display `<WindowTracker>` if `show` is `true`
-   */
+  const [show, setShow] = React.useState(true);
+
+  function toggle() {
+    setShow((prevShow) => !prevShow);
+  }
 
   return (
     <div className="container">
-      <button>Toggle WindowTracker</button>
-      <WindowTracker />
+      <button onClick={toggle}>Toggle WindowTracker</button>
+      {show && <WindowTracker />}
     </div>
   );
 }
--- 5. useEffect/2.stateandeffectpractice/src/WindowTracker.js
+++ 5. useEffect/2.stateandeffectpractice/src/WindowTracker.js
@@ -1,7 +1,13 @@
-import React from "react"
+import React from 'react';
 
 export default function WindowTracker() {
-    return (
-        <h1>Window width: {window.innerWidth}</h1>
-    )
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+
+  React.useEffect(() => {
+    window.addEventListener('resize', function () {
+      setWindowWidth(window.innerWidth);
+    });
+  }, []);
+
+  return <h1>Window width: {windowWidth}</h1>;
 }

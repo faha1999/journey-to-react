7c507b0ec18403fb650e1f413c15dcdcfe106341
faha1999 <kafaha1999@gmail.com>
2022-04-01 21:30:36 +0600

    tested & applied useEffect cleanup function

--- 5. useEffect/2.stateandeffectpractice/src/WindowTracker.js
+++ 5. useEffect/2.stateandeffectpractice/src/WindowTracker.js
@@ -4,9 +4,17 @@ export default function WindowTracker() {
   const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
 
   React.useEffect(() => {
-    window.addEventListener('resize', function () {
+    function watchWidth() {
+      console.log('Setting up...');
       setWindowWidth(window.innerWidth);
-    });
+    }
+
+    window.addEventListener('resize', watchWidth);
+
+    return function () {
+      console.log('Cleaning up...');
+      window.removeEventListener('resize', watchWidth);
+    };
   }, []);
 
   return <h1>Window width: {windowWidth}</h1>;

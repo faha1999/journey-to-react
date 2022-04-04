8d5a8643e1b2ce84acce120bd08754429df40c26
faha1999 <kafaha1999@gmail.com>
2022-04-04 16:42:00 +0600

    added dies component

--- 3.tenzies/src/App.js
+++ 3.tenzies/src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import './css/style.min.css';
+import Die from './Die';
 
 export default function App() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+      </div>
+    </main>
+  );
 }

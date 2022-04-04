36c6d8a89da6db9295537254ba86f95669a7d22a
faha1999 <kafaha1999@gmail.com>
2022-04-05 02:59:12 +0600

    added map array

--- 3. Tenzies/src/App.js
+++ 3. Tenzies/src/App.js
@@ -3,6 +3,8 @@ import './css/style.min.css';
 import Die from './Die';
 
 export default function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
@@ -11,22 +13,11 @@ export default function App() {
     return newDice;
   }
 
-  console.log(allNewDice());
+  const diceElements = dice.map((die) => <Die value={die} />);
 
   return (
     <main>
-      <div className="dice-container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-      </div>
+      <div className="dice-container">{diceElements}</div>
     </main>
   );
 }

ac75a565b209492b1d656fb5b2bf468b3028023f
faha1999 <kafaha1999@gmail.com>
2022-04-05 02:40:25 +0600

    generate array for 10 random number

--- 3. Tenzies/src/App.js
+++ 3. Tenzies/src/App.js
@@ -3,6 +3,16 @@ import './css/style.min.css';
 import Die from './Die';
 
 export default function App() {
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  console.log(allNewDice());
+
   return (
     <main>
       <div className="dice-container">

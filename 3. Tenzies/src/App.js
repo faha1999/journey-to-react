4ebd4982c0d9ca1166c2a91b72dfd9e735910035
faha1999 <kafaha1999@gmail.com>
2022-04-05 03:36:42 +0600

    added button function

--- 3. Tenzies/src/App.js
+++ 3. Tenzies/src/App.js
@@ -13,11 +13,18 @@ export default function App() {
     return newDice;
   }
 
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
   const diceElements = dice.map((die) => <Die value={die} />);
 
   return (
     <main>
       <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }

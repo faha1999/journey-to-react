823b8bc2496b6e16f346d597de2f1e8a26506a10
faha1999 <kafaha1999@gmail.com>
2022-04-07 03:41:36 +0600

    start quiz template innitilly done

--- quizzical/src/App.js
+++ quizzical/src/App.js
@@ -2,5 +2,13 @@ import React from 'react';
 import './css/style.min.css';
 
 export default function App() {
-  return <div></div>;
+  return (
+    <main>
+      <div className="start-quiz">
+        <h1>Quizzical</h1>
+        <p>Test your knowledge</p>
+        <button>Start quiz</button>
+      </div>
+    </main>
+  );
 }

96e823dec6f50bafafda632448b65e2551aa10ce
faha1999 <kafaha1999@gmail.com>
2022-04-08 02:48:42 +0600

    innitial commit

--- quizzical/src/App.js
+++ quizzical/src/App.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import StartPage from './components/StartPage';
+import QuizPage from './components/QuizPage';
+import { useState } from 'react';
 import './css/style.min.css';
 
 export default function App() {
+  const [isOnStartPage, setIsOnStartPage] = useState(true);
+  const [categories, setCategories] = useState([]);
+  const [inputData, setInputData] = useState({});
+  const [showModal, setShowModal] = useState(false);
+
+  function clickHandler() {
+    !inputData.numberOfQuestions || !inputData.category
+      ? setShowModal(true)
+      : setIsOnStartPage(!isOnStartPage);
+  }
+
+  function PlayAgain() {
+    setIsOnStartPage(true);
+    setInputData({});
+  }
+
   return (
-    <main>
-      <div className="start-quiz">
-        <h1>Quizzical</h1>
-        <p>Test your knowledge</p>
-        <button>Start quiz</button>
-      </div>
-    </main>
+    <div className="App">
+      <main className="AppMain">
+        <div className="bgYellow"></div>
+        <div className="bgBlue"></div>
+        {isOnStartPage ? (
+          <StartPage
+            categories={categories}
+            clickHandler={clickHandler}
+            inputData={inputData}
+            setCategories={setCategories}
+            setInputData={setInputData}
+            setShowModal={setShowModal}
+            showModal={showModal}
+          />
+        ) : (
+          <QuizPage inputData={inputData} PlayAgain={PlayAgain} />
+        )}
+      </main>
+    </div>
   );
 }
